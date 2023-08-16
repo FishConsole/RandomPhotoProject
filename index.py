4f42ed03482de02a39b4bd77c5bd3979e89aeb2f
Fish <2645602049@qq.com>
2023-08-16 17:58:41 +0800

    /DeepDanbooru/ /img/ /数据库/ /Lib/smtp_server.py /Lib/自动图像标签生成器.py

--- index.py
+++ index.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
     d.start()
     a.start()
 
-    main.run(host='::', port=9093, threaded=False,
+    main.run(host='164.155.203.179', port=443, threaded=False,
              ssl_context=(main.config['SSL_CERTIFICATE'], main.config['SSL_PRIVATE_KEY']))
 
     # main.run(host='::', port=, debug=True)
