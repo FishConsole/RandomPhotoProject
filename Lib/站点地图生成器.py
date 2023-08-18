39307201bf24b3c4cc233c8e0f31d5f545a3b3de
Fish <2645602049@qq.com>
2023-08-18 22:19:45 +0800

    /DeepDanbooru/ /img/ /数据库/

--- Lib/站点地图生成器.py
+++ Lib/站点地图生成器.py
@@ -18,21 +18,19 @@ def 生成站点地图(资源):
             for 链接 in 全部图片信息:
                 子Sitemap元素 = ET.SubElement(根, "sitemap")
 
-                链接元素 = ET.SubElement(子Sitemap元素, "url")
-
-                地址 = ET.SubElement(链接元素, "lastmod")
+                地址 = ET.SubElement(子Sitemap元素, "lastmod")
                 地址.text = f'https://{路径控制.启动位置.域名()}/PhotoInfo/{链接[0]}'
 
-                最后修改时间 = ET.SubElement(链接元素, "loc")
+                最后修改时间 = ET.SubElement(子Sitemap元素, "loc")
                 最后修改时间.text = 链接[1]
 
-                更新频率 = ET.SubElement(链接元素, "changefreq")
+                更新频率 = ET.SubElement(子Sitemap元素, "changefreq")
                 更新频率.text = 'never'
 
-                优先级元素 = ET.SubElement(链接元素, "priority")
+                优先级元素 = ET.SubElement(子Sitemap元素, "priority")
                 优先级元素.text = 优先级
 
-                描述 = ET.SubElement(链接元素, "description")
+                描述 = ET.SubElement(子Sitemap元素, "description")
                 描述.text = 链接[2]
 
     return ET.tostring(根, encoding="utf-8").decode("utf-8")
