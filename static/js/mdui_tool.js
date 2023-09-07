08131471ac760c87b7b42972cab7c7a0feb6eccb
Fish <2645602049@qq.com>
2023-09-07 17:26:17 +0800

    /DeepDanbooru/ /img/ static/bgm /数据库/

--- static/js/mdui_tool.js
+++ static/js/mdui_tool.js
@@ -1,9 +1,9 @@
 function 程序名() {
-    return 'Photos'
+    return 'RandomPhotos'
 }
 
 function 对话框(内容) {
-    mdui.alert(内容, '<div><img src="../../static/assats/randomphoto.svg"></div>' + 程序名());
+    mdui.alert(内容, `<div><img src="../../static/assats/randomphoto.svg"></div><div class="对话框文字">${程序名()}</div>`);
 }
 
 
